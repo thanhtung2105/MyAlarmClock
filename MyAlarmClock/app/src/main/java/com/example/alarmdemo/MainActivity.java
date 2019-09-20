fe50e5fdf0351ac1d6f3588d0ed3c0c09ab35f13
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-09-20 09:51:24 +0700

    Create MainActivity.java

---
+++ MyAlarmClock/app/src/main/java/com/example/alarmdemo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.alarmdemo;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_container, new AlarmFragment()).commit();
+    }
+}
